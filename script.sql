5e4a95d17d7ed6814581e7fe68cba9d02c1d6d0b
JaumVituu <78977800+JaumVituu@users.noreply.github.com>
2024-07-11 20:43:56 -0300

    Adiciona questão 1

--- script.sql
+++ script.sql
@@ -2,6 +2,16 @@
 -- 1 Base de dados e criação de tabela
 --escreva a sua solução aqui
 
+CREATE DATABASE prova_pbd
+
+CREATE Table tb_student_prediction(
+    student_id SERIAL PRIMARY key,
+    salary int,
+    father_education int,
+    partner int,
+    prep_exam int,
+    grade int
+);
 
 -- ----------------------------------------------------------------
 -- 2 Resultado em função da formação dos pais
