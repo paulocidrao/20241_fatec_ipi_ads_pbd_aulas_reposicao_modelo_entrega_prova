4060288bb4cd7d06b60abf8932bef2c3f9e13e40
JaumVituu <78977800+JaumVituu@users.noreply.github.com>
2024-07-11 20:44:08 -0300

    Adiciona questao 2

--- script.sql
+++ script.sql
@@ -7,16 +7,37 @@ CREATE DATABASE prova_pbd
 CREATE Table tb_student_prediction(
     student_id SERIAL PRIMARY key,
     salary int,
-    father_education int,
+    father_edu int,
+    mother_edu int,
     partner int,
     prep_exam int,
     grade int
 );
 
+
+
 -- ----------------------------------------------------------------
 -- 2 Resultado em função da formação dos pais
 --escreva a sua solução aqui
 
+DO $$
+    DECLARE
+        cur_aprovados_pais_phd REFCURSOR;
+        v_students int;
+    BEGIN
+        OPEN cur_aprovados_pais_phd FOR
+        SELECT count(studentid) from tb_student_prediction
+        WHERE father_edu = 6
+        OR mother_edu = 6;
+
+    LOOP
+        FETCH cur_aprovados_pais_phd into v_students;
+        EXIT WHEN NOT FOUND;
+        RAISE NOTICE 'QUANTIDADE DE ALUNOS APROVADOS COM PAIS PHD: %', v_students;
+    END LOOP;  
+
+    CLOSE cur_aprovados_pais_phd;
+END $$
 
 -- ----------------------------------------------------------------
 -- 3 Resultado em função dos estudos
